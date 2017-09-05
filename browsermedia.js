0d090238f7aa636c84bcb169e3b5be318ba75f53
YarkoL <yarko.dev@gmail.com>
2017-09-05 15:21:32 +0300

    :)

--- browsermedia.js
+++ browsermedia.js
@@ -1036,7 +1036,7 @@ var FrameBuffer = function() {
             var i = n.data;
             var r = new Uint8Array(i.buffer);
             return new RawFrame(r, this.mCanvasElement.width, this.mCanvasElement.height)
-        } catch (e) {
+        } catch (e) {FrameBuffer
             var r = new Uint8Array(this.mCanvasElement.width * this.mCanvasElement.height * 4);
             r.fill(255, 0, r.length - 1);
             return new RawFrame(r, this.mCanvasElement.width, this.mCanvasElement.height)
@@ -1153,84 +1153,6 @@ var MediaPeer = function(e) {
     };
     return t
 }(WebRtcDataPeer);
-
-function BrowserMediaNetwork_TestLocalCamera() {
-    FrameBuffer.DEBUG_SHOW_ELEMENTS = true;
-    var e = new NetworkConfig;
-    e.SignalingUrl = null;
-    var t = new BrowserMediaNetwork(e);
-    var n = new MediaConfig;
-    n.Audio = true;
-    n.Video = true;
-    t.Configure(n);
-    setInterval(function() {
-        t.Update();
-        var e = t.TryGetFrame(ConnectionId.INVALID);
-        console.log("width" + e.Width + " height:" + e.Height + " data:" + e.Buffer[0]);
-        t.Flush()
-    }, 50)
-}
-
-function BrowserMediaNetwork_Test2() {
-    FrameBuffer.DEBUG_SHOW_ELEMENTS = true;
-    var e = new NetworkConfig;
-    e.SignalingUrl = "wss://remotesupport.northeurope.cloudapp.azure.com:12777";
-    var t = new BrowserMediaNetwork(e);
-    var n = new BrowserMediaNetwork(e);
-    var i = new MediaConfig;
-    i.Audio = true;
-    i.Video = true;
-    setTimeout(function() {
-        t.Configure(i)
-    }, 5e3);
-    setTimeout(function() {
-        console.log("connecting network1");
-        t.StartServer("ts");
-        if (n != null) n.Configure(i)
-    }, 1e4);
-    setTimeout(function() {
-        if (n != null) {
-            console.log("connecting network2");
-            n.StartServer("ts")
-        }
-    }, 15e3);
-    var r = null;
-    var o = null;
-    setInterval(function() {
-        t.Update();
-        var e = null;
-        var i = null;
-        e = t.TryGetFrame(ConnectionId.INVALID);
-        if (e != null) console.log("local1 width" + e.Width + " height:" + e.Height + " data:" + e.Buffer[0]);
-        var a;
-        while ((a = t.Dequeue()) != null) {
-            console.log("network1: " + a.toString());
-            if (a.Type == NetEventType.NewConnection) {
-                r = a.ConnectionId
-            }
-        }
-        if (r != null) {
-            e = t.TryGetFrame(r);
-            if (e != null) console.log("remote1 width" + e.Width + " height:" + e.Height + " data:" + e.Buffer[0])
-        }
-        t.Flush();
-        if (n == null) return;
-        n.Update();
-        i = n.TryGetFrame(ConnectionId.INVALID);
-        if (i != null) console.log("local2 width" + i.Width + " height:" + i.Height + " data:" + i.Buffer[0]);
-        while ((a = n.Dequeue()) != null) {
-            console.log("network2: " + a.toString());
-            if (a.Type == NetEventType.NewConnection) {
-                o = a.ConnectionId
-            }
-        }
-        if (o != null) {
-            i = n.TryGetFrame(o);
-            if (i != null) console.log("remote2 width" + i.Width + " height:" + i.Height + " data:" + i.Buffer[0])
-        }
-        n.Flush()
-    }, 50)
-}
 
 function getParameterByName(e, t) {
     if (!t) t = window.location.href;
